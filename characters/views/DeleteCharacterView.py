4a7e92e8a29e9032353874236b45616355585cf4
Andy Wang <trollscience1589@gmail.com>
2023-04-20 08:12:40 -0400

    styled login and sign up forms

--- characters/views/DeleteCharacterView.py
+++ characters/views/DeleteCharacterView.py
@@ -9,16 +9,11 @@ from characters.models import Character
 class DeleteCharacterView(LoginRequiredMixin, DeleteView):
     login_url = reverse_lazy("accounts:login")
     model = Character
-    template_name = "characters/view.html"
+    template_name = "characters/delete.html"
     context_object_name = "char"
 
     def get_success_url(self):
         return reverse_lazy("folders:view", kwargs={"pk": self.get_object().folder.id})
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-        data["delete_confirmation"] = True
-        return data
 
     def get(self, request, pk):
         char = get_object_or_404(Character, id=pk)
